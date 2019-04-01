92ab4f44abe35cfd382bdadf7a52a2fb27adf0ca
홍주연 <juyeon@hongjuyeon-ui-macbookpro.local>
2019-04-01 22:44:20 +0900

    Initial Commit

---
+++ cgv/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  cgv
+//
+//  Created by 홍주연 on 01/04/2019.
+//  Copyright © 2019 hongjuyeon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
