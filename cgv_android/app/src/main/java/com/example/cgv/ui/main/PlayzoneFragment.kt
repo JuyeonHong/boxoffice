441088d130baa3fa017dd173ddb2db0bdfeca9f2
yanghyeonjin <guswls7081@naver.com>
2019-06-17 02:30:52 +0900

    create 5 tabs

--- cgv_android/app/src/main/java/com/example/cgv/ui/main/PlayzoneFragment.kt
+++ cgv_android/app/src/main/java/com/example/cgv/ui/main/PlayzoneFragment.kt
@@ -10,10 +10,7 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.example.cgv.R
 
-/**
- * A placeholder fragment containing a simple view.
- */
-class PlaceholderFragment : Fragment() {
+class PlayzoneFragment : Fragment() {
 
     private lateinit var pageViewModel: PageViewModel
 
@@ -25,15 +22,10 @@ class PlaceholderFragment : Fragment() {
     }
 
     override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
-        val root = inflater.inflate(R.layout.fragment_main, container, false)
-        val textView: TextView = root.findViewById(R.id.section_label)
-        pageViewModel.text.observe(this, Observer<String> {
-            textView.text = it
-        })
-        return root
+        return inflater.inflate(R.layout.fragment_playzone, container, false)
     }
 
     companion object {
@@ -48,8 +40,8 @@ class PlaceholderFragment : Fragment() {
          * number.
          */
         @JvmStatic
-        fun newInstance(sectionNumber: Int): PlaceholderFragment {
-            return PlaceholderFragment().apply {
+        fun newInstance(sectionNumber: Int): PlayzoneFragment {
+            return PlayzoneFragment().apply {
                 arguments = Bundle().apply {
                     putInt(ARG_SECTION_NUMBER, sectionNumber)
                 }
--- cgv_android/app/src/main/java/com/example/cgv/ui/main/SectionsPagerAdapter.kt
+++ cgv_android/app/src/main/java/com/example/cgv/ui/main/SectionsPagerAdapter.kt
@@ -7,8 +7,11 @@ import androidx.fragment.app.FragmentPagerAdapter
 import com.example.cgv.R
 
 private val TAB_TITLES = arrayOf(
-        R.string.tab_text_1,
-        R.string.tab_text_2
+    R.string.tab_title_home,
+    R.string.tab_title_event,
+    R.string.tab_title_store,
+    R.string.tab_title_playzone,
+    R.string.tab_title_my
 )
 
 /**
@@ -18,10 +21,27 @@ private val TAB_TITLES = arrayOf(
 class SectionsPagerAdapter(private val context: Context, fm: FragmentManager)
     : FragmentPagerAdapter(fm) {
 
-    override fun getItem(position: Int): Fragment {
+    override fun getItem(position: Int): Fragment? {
         // getItem is called to instantiate the fragment for the given page.
         // Return a PlaceholderFragment (defined as a static inner class below).
-        return PlaceholderFragment.newInstance(position + 1)
+        when(position) {
+            0 -> {
+                return HomeFragment()
+            }
+            1 -> {
+                return EventFragment()
+            }
+            2 -> {
+                return StoreFragment()
+            }
+            3 -> {
+                return PlayzoneFragment()
+            }
+            4 -> {
+                return MyFragment()
+            }
+            else -> return null
+        }
     }
 
     override fun getPageTitle(position: Int): CharSequence? {
@@ -30,6 +50,6 @@ class SectionsPagerAdapter(private val context: Context, fm: FragmentManager)
 
     override fun getCount(): Int {
         // Show 2 total pages.
-        return 2
+        return 5
     }
 }
