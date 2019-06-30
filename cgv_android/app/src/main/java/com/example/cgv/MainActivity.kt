1975e26162406487c7b52feefe86a09a5783703f
yanghyeonjin <guswls7081@naver.com>
2019-06-30 18:26:39 +0900

    add movie poster(horizontal scrollview) and movie info 작업 하는 중

--- cgv_android/app/src/main/java/com/example/cgv/MainActivity.kt
+++ cgv_android/app/src/main/java/com/example/cgv/MainActivity.kt
@@ -3,8 +3,10 @@ package com.example.cgv
 import android.os.Bundle
 import androidx.viewpager.widget.ViewPager
 import androidx.appcompat.app.AppCompatActivity
+import com.example.cgv.ui.main.MovieInfoPagerAdapter
 import com.example.cgv.ui.main.SectionsPagerAdapter
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_home.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,12 +17,7 @@ class MainActivity : AppCompatActivity() {
         val sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
         val viewPager: ViewPager = findViewById(R.id.view_pager)
         viewPager.adapter = sectionsPagerAdapter
-
         tabs.setupWithViewPager(viewPager)
 
-        // cgv logo 클릭 시 home tab 이동
-        img_logo.setOnClickListener {
-
-        }
     }
 }
